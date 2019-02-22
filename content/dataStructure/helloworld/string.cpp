921a9bc1edff424aa6c8e9aac28f461e75d008ae
KieSun <zx597813039@gmail.com>
2019-02-22 23:17:48 +0800

    string

---
+++ content/dataStructure/helloworld/string.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+int main(int argc, char *argv[]) {
+	string name;
+	cin >> name;
+	cout << "hello, " << name << endl;
+	cout << name.size();
+	return 0; 
+}
